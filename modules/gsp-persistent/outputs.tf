ae761ef6ac67a91d39a86dee2ad120abe75e6bec
David McDonald <idavidmcdonald@me.com>
2019-02-25 15:25:31 +0000

    wip

--- modules/gsp-persistent/outputs.tf
+++ modules/gsp-persistent/outputs.tf
@@ -2,12 +2,12 @@ output "private-subnet-ids" {
   value = ["${aws_subnet.cluster-private.*.id}"]
 }
 
-output "cert_pem" {
-  description = "Sealed secrets certificate"
-  value       = "${tls_self_signed_cert.sealed-secrets-certificate.cert_pem}"
-}
-
 output "private_key_pem" {
   description = "Sealed secrets private key"
   value       = "${tls_private_key.sealed-secrets-key.private_key_pem}"
+}
+
+output "cert_pem" {
+  description = "Sealed secrets certificate"
+  value       = "${tls_self_signed_cert.sealed-secrets-certificate.cert_pem}"
 }
