08733dc099c1866a8efff6e537be60e968befa69
David Povey <david.povey@digital.cabinet-office.gov.uk>
2019-02-25 16:28:24 +0000

    Work-in-progress. Remove vpc and subnets from this module.
    Pair: @poveyd @idavidmcdonald

--- modules/gsp-persistent/outputs.tf
+++ modules/gsp-persistent/outputs.tf
@@ -1,7 +1,3 @@
-output "private-subnet-ids" {
-  value = ["${aws_subnet.cluster-private.*.id}"]
-}
-
 output "private_key_pem" {
   description = "Sealed secrets private key"
   value       = "${tls_private_key.sealed-secrets-key.private_key_pem}"
